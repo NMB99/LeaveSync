a6ef735a7e777d404bbf799637af35ed70e5d349
Nilay Bhaisare <nilaybhaisare1098@gmail.com>
2026-06-04 14:17:51 +0100

    chore: scaffold Spring Boot project with initial dependencies

---
+++ src/main/java/com/leavesync/leavesync/LeavesyncApplication.java
@@ -0,0 +1,13 @@
+package com.leavesync.leavesync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeavesyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeavesyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leavesync/leavesync/LeavesyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leavesync.leavesync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeavesyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
