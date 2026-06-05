ae58354169b94b28cfad06d628c0356d5497b99e
Nilay Bhaisare <nilaybhaisare1098@gmail.com>
2026-06-05 18:09:59 +0100

    feat: add global exception handling with custom exceptions and error response

---
+++ src/main/java/com/leavesync/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.leavesync.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/leavesync/exception/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package com.leavesync.exception;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
